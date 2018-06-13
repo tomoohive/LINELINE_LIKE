3ee2e765f16c0e38bf604c0701213f2f1cb1906d
tomoohive <graphelate@gmail.com>
2018-06-13 14:11:34 +0900

    Finish making Home

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "LINE LIKE App"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title","Home | #{@base_title}"
   end
 
   test "should get help" do
